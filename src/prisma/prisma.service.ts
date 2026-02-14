61cb3b6e25379ea1efcd3271eb35969a48652e3d
fiston <kalambayifiston7@gmail.com>
2026-02-14 12:42:36 +0100

    feat: initial backend setup with NestJS + Prisma + Neon

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'prisma/generated/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaNeon({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({ adapter });
+  }
+}
